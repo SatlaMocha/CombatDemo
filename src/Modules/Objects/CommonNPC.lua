c5cb76eb4d2d90c56c2369d576793e4ba45eb1c0
Omer Agniho <omerag90@gmail.com>
2022-04-16 03:01:26 +0200

    Initial Commit Change To CommonNPC Objects  add new functions

--- src/Modules/Objects/CommonNPC.lua
+++ src/Modules/Objects/CommonNPC.lua
@@ -1,29 +1,56 @@
 -- [Services]
 local PFS = game:GetService"PathfindingService";
 local RS = game:GetService"RunService"
+local RE = game:GetService"ReplicatedStorage"
 
+-- [Modules]
+local Signal = require(RE.Objects:WaitForChild("Signal"))
 
 local CommonNPC = {};
 CommonNPC.__index = CommonNPC;
 
 function CommonNPC.new (char : Model)
 	local self = setmetatable({
+		-- Properties
 		Character = char;
 		CurrentWayPoint = nil;
 		CurrentDestination = nil;
+		CurrentPath = nil;
+		
+		-- Holders
+		_connections = {};
 
+		-- Events
+		ArrivedAtDestination = Signal.new();
 	}, CommonNPC)
 
 	return self;
 end
 
+
+-- Common NPC Functions
 function CommonNPC:Update ()
-	
+	self._connections["Heartbeat"] = RS.Heartbeat:Connect(function()
+
+	end)
 end
 
 function CommonNPC:SetDestination ()
 
 end
+
+function CommonNPC:Talk ()
+	
+end
+
+function CommonNPC:ShowCharacter ()
+	
+end
+
+function CommonNPC:HideCharacter ()
+
+end
+
 
 
 return CommonNPC
